b27b6ad7d6c93695482dfc63f44c937b7145a8b7
Quentin mendel <quentin.mendel1@gmail.com>
2023-02-24 14:44:48 +0100

    sdfsd

--- front/src/index.php
+++ front/src/index.php
@@ -1,5 +1,12 @@
 <?php
 
-    echo 'FRONT';
-
+    $api_url = "";
+    $users = json_decode(file_get_contents($api_url));
 ?>
+
+
+<?php
+foreach ($users as $user): ?>
+	<p><?= $user->username ?></p>
+	<p><?= $user->email ?></p>
+<?php endforeach; ?>
